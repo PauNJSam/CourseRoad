d7708994f8e49bfa06cc80deb6d9917a6a11dfbb
PauNJSam <pnj5sambueno@gmail.com>
2023-09-17 13:23:12 +0800

    initialized firebase config

---
+++ courseroad/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3snAYEcq1iIXnKbg_WsyqllouMmIr5Go",
+  authDomain: "courseroad-sofdev3.firebaseapp.com",
+  projectId: "courseroad-sofdev3",
+  storageBucket: "courseroad-sofdev3.appspot.com",
+  messagingSenderId: "29953580726",
+  appId: "1:29953580726:web:a8ef041c8a527c72e31017"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
