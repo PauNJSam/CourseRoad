ada03fe456a0189cb2440d68e08221e826929ba6
jadcey <taekagay@gmail.com>
2023-09-29 00:41:14 +0800

    landing page ongoing

--- courseroad/src/components/LandingPage.js
+++ courseroad/src/components/LandingPage.js
@@ -1,11 +1,62 @@
 import React from 'react';
-import '../styles/LandingPage.css'
+import '../styles/LandingPage.css';
+
+import logo from '../images/courseroad_logo.png';
+import landingpageicon from '../images/landingpageicon.png';
+
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
         <React.Fragment>
-
+            <div className="desktop">
+                <div className="div">
+                    <img className="pngtreecartoon-man" alt="Pngtreecartoon man" src={landingpageicon} />
+                    <div className="text-wrapper">BEST ONLINE COURSES</div>
+                        <p className="everyone-agrees-with">
+          Everyone agrees with the fact that learning management systems are a <br />
+          tremendous way to expand learners’ knowledge base and help staff enhance their skills
+                        </p>
+                    <button className="rectangle">Enroll Now</button>        
+                        <img
+                          className="copy-of-software"
+                          alt="Copy of software"
+                          src={logo}
+                        />
+                </div>
+            </div>
+            <div className="desktop">
+                <div className="div">
+                    <img className="pngtreecartoon-man" alt="Pngtreecartoon man" src={landingpageicon} />
+                    <div className="text-wrapper">BEST ONLINE COURSES</div>
+                        <p className="everyone-agrees-with">
+          Everyone agrees with the fact that learning management systems are a <br />
+          tremendous way to expand learners’ knowledge base and help staff enhance their skills
+                        </p>
+                    <button className="rectangle">Enroll Now</button>        
+                        <img
+                          className="copy-of-software"
+                          alt="Copy of software"
+                          src={logo}
+                        />
+                </div>
+            </div>
+            <div className="desktop">
+                <div className="div">
+                    <img className="pngtreecartoon-man" alt="Pngtreecartoon man" src={landingpageicon} />
+                    <div className="text-wrapper">BEST ONLINE COURSES</div>
+                        <p className="everyone-agrees-with">
+          Everyone agrees with the fact that learning management systems are a <br />
+          tremendous way to expand learners’ knowledge base and help staff enhance their skills
+                        </p>
+                    <button className="rectangle">Enroll Now</button>        
+                        <img
+                          className="copy-of-software"
+                          alt="Copy of software"
+                          src={logo}
+                        />
+                </div>
+            </div>          
         </React.Fragment>
     );
 };
