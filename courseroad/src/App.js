7b0736509af13c236f5714861b97cb9617bdbdb4
Nathaniel Arbasto <nathanielrbas2@gmail.com>
2023-09-28 21:46:15 +0800

    Revert "signIn Page"
    This reverts commit 09b63fc62ca27d0146f5c5fe5b66581570d6c697.

--- courseroad/src/App.js
+++ courseroad/src/App.js
@@ -14,7 +14,6 @@ import AdminSignIn from './components/AdminSignIn';
 import AdminCourseReports from './components/AdminCourseReports';
 import AdminTeacherApplications from './components/AdminTeacherApplications';
 import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
 
 
 const router = createBrowserRouter(
@@ -23,7 +22,7 @@ const router = createBrowserRouter(
       
       <Route index element={<LandingPage />}></Route>
       <Route path="signup" element={<SignUp />}></Route>
-      <Route path="signin" element={<SignIn />}></Route>
+
       <Route path="dashboard" element={<DashboardLayout />}>
         <Route path="teacherHome" element={<TeacherHome />}></Route>
         <Route path="createcourse" element={<CreateCourse />}></Route>
