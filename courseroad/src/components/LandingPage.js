d12cada8a3beb870ff3804ccfabcf1b452998413
jadcey <taekagay@gmail.com>
2023-09-28 22:34:21 +0800

    no changes made

--- courseroad/src/components/LandingPage.js
+++ courseroad/src/components/LandingPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import '../styles/LandingPage.css'
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
         <React.Fragment>
-            <p>This is the Landing Page</p>
-            <h1>Hi</h1>
+
         </React.Fragment>
     );
 };
