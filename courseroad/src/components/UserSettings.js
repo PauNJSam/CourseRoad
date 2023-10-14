8b4e6ced5c589e9bd3b13590283bbfcccc443d3b
jadcey <taekagay@gmail.com>
2023-10-14 08:02:58 +0800

    not yet responsive

--- courseroad/src/components/UserSettings.js
+++ courseroad/src/components/UserSettings.js
@@ -32,37 +32,6 @@ const UserSettings = () => {
         console.log("User logged in: ", loggedInEmail);
     }, []);
     
-
-    // const setProfilePic = () => {
-    //     if(profilePicUpload == null) return;
-    //     console.log("User logged in: ", loggedInEmail);
-
-    //     const imageRef = ref(storage, `profilePictures/${profilePicUpload.name + crypto.randomUUID()}`);
-    //     uploadBytes(imageRef, profilePicUpload).then(()=>{
-    //         alert("Profile Pic Uploaded");
-    //         getDownloadURL(imageRef).then(async (url)=>{
-    //             // setProfilePicURL(url);
-    //             console.log("The picture URL: ",url);
-    //             if(loggedInEmail!==undefined){
-    //                 const userDocRef = doc(db, "USERS", loggedInEmail)
-                
-    //                 await updateDoc(userDocRef, {
-    //                     profilePic: url
-    //                 }).then(()=>{
-    //                     setDp(url);
-    //                     console.log("Successfully updated profile pic");
-    //                 }).catch((error) => {
-    //                     console.error('Error updating profile pic', error);
-    //                 });
-    //             }
-    //         }).catch((error) => {
-    //             console.error('Error getting image URL: ', error);
-    //         });
-            
-    //     }).catch((error) => {
-    //         console.error('Error uploading image: ', error);
-    //     });
-    // };
     const setProfilePic = () => {
         if (profilePicUpload === null) return;
     
@@ -94,48 +63,6 @@ const UserSettings = () => {
     
 
     return(
-        // <section className="userSettings">
-        //         <div className="desktop">
-        //             <div className="frame-wrapper">
-        //             <div className="frame">
-        //             <div className="overlap">
-        //             <div className='userSettings__profile-pic'>
-        //                 <img src={dp} alt='profile' />
-        //             </div>
-        //                 <div className="text-wrapper">John Doe</div>
-        //                 <div className="div">Student</div>
-        //                 <p>Edit Profile Picture</p>
-        //                 <EditIcon onClick={()=>setShowUploadInput(!showUploadInput)}></EditIcon>
-        //                 {
-        //                     showUploadInput && <div>
-        //                         <input type="file" onChange={(event)=> {setProfilePicUpload(event.target.files[0])}}></input>
-        //                         <button type="button" onClick={setProfilePic}>Set as Profile Picture</button>
-        //                     </div>
-        //                 }
-        //             </div>
-        //             <div className="text-wrapper-2">John Doe</div>
-        //             <div className="rectangle" />
-        //             <div className="rectangle-2" />
-        //             <div className="rectangle-3" />
-        //             <div className="text-wrapper-3">Account Settings</div>
-        //             <div className="text-wrapper-4">Username:</div>
-        //             <div className="text-wrapper-5">New password:</div>
-        //             <div className="text-wrapper-6">Change password</div>
-        //             <p className="p">Create your own course! Join Us Now!</p>
-        //             <div className="text-wrapper-7">Confirm password:</div>
-        //             <div className="overlap-group">
-        //                 <div className="text-wrapper-8">Save</div>
-        //             </div>
-        //             <div className="div-wrapper">
-        //                 {/* <p className="text-wrapper-9">Apply to be a teacher</p> */}
-        //                 <button className="text-wrapper-9" onClick={() => setOpenModal(true)} >Apply To Be a Teacher</button>
-        //             </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <TeacherApplicationForm open={openModal} close={() => setOpenModal(false)} />
-        // </section>
-
         <section>
             <div className= "userSettings">
                 <div className = "desktop">
